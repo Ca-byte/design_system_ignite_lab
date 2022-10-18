36f968ac8863b571b8e4ad20f7bd228524c9b339
Ca-byte <carolinesanvieira@gmail.com>
2022-10-18 21:10:57 +0200

    Figma tokens colors and font sizes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './styles/global.css';
 export function App() {
 
   return (
-    <h1>Hello Peach</h1>
+    <h1 className='text-2xl bg-blue-700'>Hello Peach</h1>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,35 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontSize:{
+      xs:14,
+      sm:16,
+      md:18,
+      lg:20,
+      xl:24,
+      '2xl':32,
+
+
+    },
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-600': '#7C7C8A',
+      'gray-500': '#8E9090',
+      'gray-400': '#C4C4CC',
+      'gray-200': '#E1E1E6',
+
+      'grass-800': '#0E955E',
+      'grass-500': '0ED282',
+
+      'blue-700': '#005083',
+
+      'yellow-800': '#FDD420',
+
+      'red-500': 'D95E5C'
+
+
+    },
     extend: {
       fontFamily:{
         sans: 'Inter, sans-serif'
