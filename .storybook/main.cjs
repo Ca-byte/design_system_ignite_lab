615f29d08cb4d2dec4c5b3064f27dfd5169cc91b
Caroline Vieira <caroline@fetch.ly>
2022-10-19 23:21:44 +0200

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) =>{
+    if (configType === 'PRODUCTION') {
+      config.base = '/design_system_ignite_lab/'
+    }
+    return config
   }
 }
