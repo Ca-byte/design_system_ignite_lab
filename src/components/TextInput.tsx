a05a33548441645f305c7954abc2d1bb4a194ca6
Caroline Vieira <caroline@fetch.ly>
2022-10-19 19:54:29 +0200

    Input variant without icon

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -7,7 +7,7 @@ export interface TextInputRootProps{
 }
 function TextInputRoot({children}: TextInputRootProps){
   return(
-    <div className='flex items-center gap-3 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-grass-500'>
+    <div className='flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-grass-500'>
       {children}
     </div>
   )
